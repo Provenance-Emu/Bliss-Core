b0e3e5ebc4fdcca04cd41193f274fe99d957caa4
Joseph Mattiello <git@joemattiello.com>
2024-12-23 20:37:54 -0500

    tvos 16
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     name: "PVCoreBliss",
     platforms: [
         .iOS(.v16),
-        .tvOS(.v15),
+        .tvOS(.v16),
         .watchOS(.v9),
         .macOS(.v11),
         .macCatalyst(.v17),
